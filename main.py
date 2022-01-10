1cf81ee24ec080fc078960b0f1c5d0848d162c46
Suhel-Kap <suhelkapadia2@gmail.com>
2022-01-10 15:31:59 +0530

    Code refactored

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from flask import render_template, request
 import requests
 import json
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+API_KEY = os.getenv("API_KEY")
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         city = request.form['city']
         params = {
-            'api_key': 'ca39f170e71c8087182831e20a2ac201',
+            'api_key': API_KEY,
             'city_name': city
         }
         url = requests.get(
@@ -26,20 +28,6 @@ def home():
         temp = data['main']['temp']
         return render_template('weather.html', data=data,my_string=my_string, city_name=city_name, temp=temp, country_name=country_name)
     return render_template('index.html')
-
-@app.route("/result", methods=['GET', 'POST'])
-def result():
-    data = []
-    error = None
-    select = request.form.get('comp_select')
-    resp = query_api(select)
-    pp(resp)
-    if resp:
-        data.append(resp)
-        if len(data) != 2:
-            error = 'Bad Response from Weather API'
-            return render_template('result.html', data=data, error=error)
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
